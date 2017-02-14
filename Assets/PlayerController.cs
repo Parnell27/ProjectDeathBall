6e79de84ac8f8331d32fd41ffae5540c6f52d006
Parnell27 <jakegp@live.co.uk>
2017-02-14 16:47:15 +0000

    Set up network join and host for clients.
    Clients can now host games or join others. These player will appear
    within the same scene and are capable of movement. Synchronised movement
    is not yet working.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour
+{
 
 	// Use this for initialization
 	void Start () {
@@ -11,19 +13,22 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (!isLocalPlayer)
+            return;
+
+        PlayerMovement();
 	}
 
     public float moveSpeed = 8f; //Default movement speed
     
     void PlayerMovement()
     {
-        //Change float value to get key functions for wasd controls
-
         float moving = Input.GetAxis("Vertical") * moveSpeed; 
         //Creates movement in the direction the vertcal and horizontail axes are facing.
         float strafing = Input.GetAxis("Horizontal") * moveSpeed;
         moving *= Time.deltaTime;
         strafing *= Time.deltaTime;
+        transform.Translate(strafing, 0, moving);
     }
 }
