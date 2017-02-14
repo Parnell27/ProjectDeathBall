53ed849fe628f287f3fbb47a007c44e9bc07924a
Parnell27 <jakegp@live.co.uk>
2017-02-14 13:57:09 +0000

    Recreated player prefab with camera and rigidbody.
    The player prefab now includes a camera object as a child. I have also
    added a rigid body to the prefab.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -13,4 +13,17 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public float moveSpeed = 8f; //Default movement speed
+    
+    void PlayerMovement()
+    {
+        //Change float value to get key functions for wasd controls
+
+        float moving = Input.GetAxis("Vertical") * moveSpeed; 
+        //Creates movement in the direction the vertcal and horizontail axes are facing.
+        float strafing = Input.GetAxis("Horizontal") * moveSpeed;
+        moving *= Time.deltaTime;
+        strafing *= Time.deltaTime;
+    }
 }
