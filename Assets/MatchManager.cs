0ebccbc45e0d05760631d5de72186cbd54190c72
Alex231 <a13x231mcsl@gmail.com>
2017-05-07 19:17:04 +0100

    Fixed Scoring

--- Assets/MatchManager.cs
+++ Assets/MatchManager.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class MatchManager : NetworkBehaviour {
+public class MatchManager : MonoBehaviour {
 
-    public int team1Score;
+	public static MatchManager singleton;
+
+	public void Awake()
+	{
+		singleton = this;
+	}
+
+	public void OnDestroy()
+	{
+		singleton = null;
+	}
+
+	public int team1Score;
     public int team2Score;
+
+	void ResetScores()
+	{
+		team1Score = team2Score = 0;
+	}
 
     // Use this for initialization
     void Start () {
-        team1Score = 0;
-        team2Score = 0;
+		ResetScores();
 	}
 	
 	// Update is called once per frame
